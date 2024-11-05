8497a47fd9707ba56756d2d3f34c5b5a27b576f0
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-11-05 12:18:40 -0300

    Add description and version

--- app/main.py
+++ app/main.py
@@ -3,7 +3,12 @@ from app.lifespan import app_lifespan
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="BircleAI Technical Test", lifespan=app_lifespan)
+app = FastAPI(
+    title="BircleAI Technical Test",
+    description="This is the API documentation for the BircleAI Technical Test.",
+    version="0.1",
+    lifespan=app_lifespan,
+)
 
 # Add CORS middleware to allow requests from any origin.
 app.add_middleware(
