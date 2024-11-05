a59b2fdb7f29a3e0ed8168fc99a1b7aaa781904f
Heber Damián Alturria <heber.alturria.git@gmail.com>
2024-11-05 12:01:54 -0300

    Add docstings in the llama index singleton

--- app/models/llama_index_singleton.py
+++ app/models/llama_index_singleton.py
@@ -1,5 +1,5 @@
 from app.constants.llama_index_constants import GROQ_MODEL, EMBEDDING_MODEL, DATA_PATH
-from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader
+from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader, Document
 from llama_index.core.query_engine import BaseQueryEngine
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
@@ -17,7 +17,15 @@ class _LlamaIndexSingleton:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def _get_documents(self):
+    def _get_documents(self) -> list[Document]:
+        """Load documents from the specified directory using a FlatReader.
+
+        Returns:
+            list[Document]: A list of documents loaded from the directory.
+
+        Raises:
+            ValueError: If no files are found in the directory or if the directory does not exist.
+        """
         parser = FlatReader()
 
         file_extractor = {".txt": parser}
@@ -29,6 +37,14 @@ class _LlamaIndexSingleton:
         return documents
 
     def _configure_llm(self):
+        """Configure the language model (LLM) using the Groq API key.
+
+        This method initializes the Groq model with the provided API key, which must be set
+        in the environment variables.
+
+        Raises:
+            RuntimeError: If the GROQ_API_KEY is not set in the environment variables.
+        """
         api_key = os.getenv("GROQ_API_KEY")
 
         if api_key is None:
@@ -38,23 +54,46 @@ class _LlamaIndexSingleton:
         Settings.llm = groq_llm
 
     def _configure_embedding(self):
+        """
+        Configure the embedding model using Hugging Face's embedding model.
+        """
         hugginface_embedding = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL)
         Settings.embed_model = hugginface_embedding
 
     def initialize(self):
+        """
+        Initialize the Llama index by configuring the LLM, embedding, and loading documents.
+        """
         self._configure_llm()
         self._configure_embedding()
         documents = self._get_documents()
         self._index = VectorStoreIndex.from_documents(documents, show_progress=True)
 
     def get_query_engine(self) -> BaseQueryEngine:
+        """Retrieve the query engine for the Llama index.
+
+        Returns:
+            BaseQueryEngine: The query engine instance for querying the Llama index.
+
+        Raises:
+            RuntimeError: If the index has not been initialized.
+        """
         if self._index is None:
             raise RuntimeError("Index is not initialized.")
         return self._index.as_query_engine()
 
 
 def provide_query_engine() -> BaseQueryEngine:
+    """Provide the query engine from the Llama index singleton.
+
+    Returns:
+        BaseQueryEngine: The query engine instance.
+    """
     return _LlamaIndexSingleton().get_query_engine()
 
+
 def initilize_llama_index():
+    """
+    Initialize the Llama index singleton instance and index the documents.
+    """
     _LlamaIndexSingleton().initialize()
